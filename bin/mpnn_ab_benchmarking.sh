6aa1d1478d5596489807c51bef417dc26868fa17
varun-shanker <vrs.stanford.edu>
2024-07-04 01:34:25 +0000

    mpnn benchmarking fpath

--- bin/mpnn_ab_benchmarking.sh
+++ bin/mpnn_ab_benchmarking.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Function to run ProteinMPNN
+# # Function to run ProteinMPNN
 run_protein_mpnn() {
-    python ProteinMPNN/protein_mpnn_run.py \
+    python ../ProteinMPNN/protein_mpnn_run.py \
         --path_to_fasta "$1" \
         --score_only 1 \
         --save_score 1 \
@@ -13,28 +13,28 @@ run_protein_mpnn() {
 
 # G6 Light Chain
 run_protein_mpnn \
-    "structural-evolution/data/mutagenesis_expts/g6/g6_2fjg_lc_lib.fasta" \
+    "data/ab_mutagenesis_expts/g6/g6_2fjg_lc_lib.fasta" \
     "L" \
-    "structural-evolution/output/mutagenesis_expts/g6/proteinMpnnLC" \
-    "structural-evolution/data/mutagenesis_expts/g6/2fjg_vlh_fvar.pdb"
+    "output/ab_mutagenesis_expts/g6/proteinMpnnLC" \
+    "data/ab_mutagenesis_expts/g6/2fjg_vlh_fvar.pdb"
 
 # G6 Heavy Chain
 run_protein_mpnn \
-    "structural-evolution/data/mutagenesis_expts/g6/g6_2fjg_hc_lib.fasta" \
+    "data/ab_mutagenesis_expts/g6/g6_2fjg_hc_lib.fasta" \
     "H" \
-    "structural-evolution/output/mutagenesis_expts/g6/proteinMpnnHC" \
-    "structural-evolution/data/mutagenesis_expts/g6/2fjg_vlh_fvar.pdb"
+    "output/ab_mutagenesis_expts/g6/proteinMpnnHC" \
+    "data/ab_mutagenesis_expts/g6/2fjg_vlh_fvar.pdb"
 
 # CR6261 Heavy Chain
 run_protein_mpnn \
-    "structural-evolution/data/mutagenesis_expts/cr6261/cr6261_3gbn_hc_lib.fasta" \
+    "data/ab_mutagenesis_expts/cr6261/cr6261_3gbn_hc_lib.fasta" \
     "H" \
-    "structural-evolution/output/mutagenesis_expts/cr6261/mpnn" \
-    "structural-evolution/data/mutagenesis_expts/cr6261/3gbn_ablh_fvar.pdb"
+    "output/ab_mutagenesis_expts/cr6261/mpnn" \
+    "data/ab_mutagenesis_expts/cr6261/3gbn_ablh_fvar.pdb"
 
 # CR9114 Heavy Chain
 run_protein_mpnn \
-    "structural-evolution/data/mutagenesis_expts/cr9114/cr9114_4fqi_hc_lib.fasta" \
+    "data/ab_mutagenesis_expts/cr9114/cr9114_4fqi_hc_lib.fasta" \
     "H" \
-    "structural-evolution/output/mutagenesis_expts/cr9114/mpnn" \
-    "structural-evolution/data/mutagenesis_expts/cr9114/4fqi_ablh_fvar.pdb"
+    "output/ab_mutagenesis_expts/cr9114/mpnn" \
+    "data/ab_mutagenesis_expts/cr9114/4fqi_ablh_fvar.pdb"
