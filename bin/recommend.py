f3b443999a68a623247a90dc32e6df065d77ed58
KrajShuffle <kraj21@berkeley.edu>
2024-09-19 17:37:29 -0700

    Added code to conduct combinatorial analysis on beneficial mutations identified from csv file

--- bin/recommend.py
+++ bin/recommend.py
@@ -6,7 +6,8 @@ import torch
 import torch.nn.functional as F
 from tqdm import tqdm
 import os
-
+import itertools
+import re
 import esm
 from dms_utils import deep_mutational_scan
 import warnings
@@ -17,37 +18,87 @@ import multichain_util
 import score_log_likelihoods
 
 def get_native_seq(pdbfile, chain):
-        structure = util.load_structure(pdbfile, chain)
-        _ , native_seq = util.extract_coords_from_structure(structure)
-        return native_seq
-    
+    structure = util.load_structure(pdbfile, chain)
+    _, native_seq = util.extract_coords_from_structure(structure)
+    return native_seq
+
+def create_all_combos(mutations: list):
+    '''Create list of all potential combos of mutations'''
+    pot_combos = []
+    for num_combined in range(2, len(mutations) + 1):
+        num_pot_combos = list(itertools.combinations(mutations, num_combined))
+        pot_combos += num_pot_combos
+    return pot_combos
+
+def create_dict_seqs(all_combos: list, wt_chain_seq: str, offset=0):
+    '''Create dictionary of mutations separated by _ as key and mutated seqs as values'''
+    dict_seqs = {}
+    for mutation_combo in all_combos:
+        seq_copy = list(wt_chain_seq)
+        for mutation in mutation_combo:
+            _, mut_loc, mut_pot_aa = re.match('([A-Z])(\d+)([A-Z])', mutation).groups()
+            seq_copy[int(mut_loc) - offset] = mut_pot_aa
+        mutated_seq = ''.join(seq_copy)
+        dict_key = '_'.join(mutation_combo)
+        dict_seqs[dict_key] = mutated_seq
+
+    return dict_seqs
+
 def write_dms_lib(args):
-    '''Writes a deep mutational scanning library, including the native/wildtype (wt) of the 
+    '''Writes a deep mutational scanning library, including the native/wildtype (wt) of the
         indicated target chain in the structure to an output Fasta file'''
 
     sequence = get_native_seq(args.pdbfile, args.chain)
-    Path(args.seqpath).parent.mkdir(parents=True, exist_ok=True)
-    with open(args.seqpath, 'w') as f:
-        f.write('>wt\n')
-        f.write(sequence+'\n')
-        for pos, wt, mt in deep_mutational_scan(sequence):
-            assert(sequence[pos] == wt)
-            mut_seq = sequence[:pos] + mt + sequence[(pos + 1):]
-            f.write('>' + str(wt) + str(pos+1+args.offset) + str(mt) + '\n')
-            f.write(mut_seq + '\n')
+    # If interested in testing all variants of wt sequence, each with a single substitution
+    if args.mutpath == None:
+        Path(args.seqpath).parent.mkdir(parents=True, exist_ok=True)
+        with open(args.seqpath, 'w') as f:
+            f.write('>wt\n')
+            f.write(sequence + '\n')
+            for pos, wt, mt in deep_mutational_scan(sequence):
+                assert (sequence[pos] == wt)
+                mut_seq = sequence[:pos] + mt + sequence[(pos + 1):]
+                f.write('>' + str(wt) + str(pos + 1 + args.offset) + str(mt) + '\n')
+                f.write(mut_seq + '\n')
+    # If interested in testing all combinations of "beneficial" substitutions to wt sequence
+    else:
+        # Load in and extract list of beneficial mutations to do mutation combinatorial analysis
+        df_mutations = pd.read_csv(args.mutpath)
+        mutation_col_name = df_mutations.columns[0]
+        chain_mutations = df_mutations[mutation_col_name].values
+
+        # Create list of all potential combos of mutations ranging from double to all mutations on same chain
+        all_pot_combos = create_all_combos(chain_mutations)
+        print("Number of All Potential Combos:", len(all_pot_combos))
+
+        # Create dictionary of all mutations labeled sep by _ and mutated seqs as values
+        dict_chain_seqs = create_dict_seqs(all_combos=all_pot_combos, wt_chain_seq=sequence, offset=args.offset + 1)
+
+        # Write file containing all combos of mutations and corresponding mutated seqs
+        Path(args.seqpath).parent.mkdir(parents=True, exist_ok=True)
+        with open(args.seqpath, 'w') as f:
+            f.write('>wt\n')
+            f.write(sequence + '\n')
+            for mutation_name, mutated_seq in dict_chain_seqs.items():
+                f.write(">" + mutation_name + "\n" + mutated_seq + "\n")
 
 def get_top_n(args):
     recs, rec_inds = [], []
-    scores_df = pd.read_csv(args.outpath).sort_values(by = 'log_likelihood', ascending = False)
+    scores_df = pd.read_csv(args.outpath).sort_values(by='log_likelihood', ascending=False)
 
-    for seqid in scores_df['seqid']:
-        res_ind = seqid[1:-1]
-        if (rec_inds.count(res_ind) < args.maxrep): 
-            if args.upperbound == None or (int(res_ind) < int(args.upperbound)):
-                recs.append(seqid)
-                rec_inds.append(res_ind)
-        if len(recs) == args.n:
-            break 
+    # If outputting the top 10 single substitutions to wildtype sequence
+    if args.mutpath == None:
+        for seqid in scores_df['seqid']:
+            res_ind = seqid[1:-1]
+            if (rec_inds.count(res_ind) < args.maxrep):
+                if args.upperbound == None or (int(res_ind) < int(args.upperbound)):
+                    recs.append(seqid)
+                    rec_inds.append(res_ind)
+            if len(recs) == args.n:
+                break
+                # If outputting the top 10 beneficial mutation combos to the wildtype sequence
+    else:
+        recs = scores_df.head(args.n)['seqid']
 
     print(f'\n Chain {args.chain}')
     print(*recs, sep='\n')
@@ -55,7 +106,7 @@ def get_top_n(args):
 def get_model_checkpoint_path(filename):
     # Expanding the user's home directory
     return os.path.expanduser(f"~/.cache/torch/hub/checkpoints/{filename}")
-        
+
 def main():
     parser = argparse.ArgumentParser(
         description='Score sequences based on a given structure.'
@@ -66,7 +117,11 @@ def main():
     )
     parser.add_argument(
         '--seqpath', type=str,
-        help='filepath where fasta of dms library should be saveda',
+        help='filepath where fasta of dms library should be saved',
+    )
+    parser.add_argument(
+        '--mutpath', type=str, default=None,
+        help='filepath of csv with beneficial mutations'
     )
     parser.add_argument(
         '--outpath', type=str,
@@ -87,18 +142,18 @@ def main():
         help='use the backbone of only target chain in the input for conditioning'
     )
     parser.add_argument(
-            '--order', type=str, default=None,
-            help='for multichain, option to specify order of chains'
+        '--order', type=str, default=None,
+        help='for multichain, option to specify order of chains'
     )
     parser.add_argument(
         '--n', type=int,
-        help='number of desired predictions to be output', 
+        help='number of desired predictions to be output',
         default=10,
     )
     parser.add_argument(
         '--maxrep', type=int,
         help='maximum representation of a single site in the top recommendations  \
-              (eg: maxrep = 1 is a unique set where no wildtype residue is mutated more than once)', 
+              (eg: maxrep = 1 is a unique set where no wildtype residue is mutated more than once)',
         default=1,
     )
     parser.add_argument(
@@ -109,11 +164,11 @@ def main():
     parser.add_argument(
         '--upperbound', type=int,
         help='only residue positions less than the user-defined upperbound are considered to be recommended for screening \
-                (but all positions are still conditioned for scoring)', 
+                (but all positions are still conditioned for scoring)',
         default=None,
     )
     parser.add_argument(
-        "--nogpu", action="store_true", 
+        "--nogpu", action="store_true",
         help="Do not use GPU even if available"
     )
 
@@ -125,17 +180,17 @@ def main():
     if args.outpath is None:
         args.outpath = f'output/{args.pdbfile[:-4]}-chain{args.chain}_scores.csv'
 
-    #write dms library for target chain 
+    # write dms library for target chain
     write_dms_lib(args)
 
     model_checkpoint_path = get_model_checkpoint_path('esm_if1_20220410.pt')
     with warnings.catch_warnings():
-            warnings.simplefilter('ignore', UserWarning)
-            model, alphabet = esm.pretrained.load_model_and_alphabet( \
-                model_checkpoint_path \
+        warnings.simplefilter('ignore', UserWarning)
+        model, alphabet = esm.pretrained.load_model_and_alphabet( \
+            model_checkpoint_path \
             )
     model = model.eval()
-    
+
     if args.multichain_backbone:
         score_log_likelihoods.score_multichain_backbone(model, alphabet, args)
     else:
@@ -143,6 +198,5 @@ def main():
 
     get_top_n(args)
 
-
 if __name__ == '__main__':
-        main()
+    main()
